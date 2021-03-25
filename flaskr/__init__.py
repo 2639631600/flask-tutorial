bcb8e4fb2a65b9161f37c4eb48c62a97f4398a5a
2639631600 <2639631600@qq.com>
2021-03-25 23:06:33 +0800

    添加蓝图注释

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,9 +31,11 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # 注册auth蓝图
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # 注册blog蓝图
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
